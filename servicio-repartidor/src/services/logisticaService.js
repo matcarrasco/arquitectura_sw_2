769fdf103a107e04fde8a9b006ac124edcaa6095
nachoskill <167713917+nachoskill@users.noreply.github.com>
2026-06-18 23:25:35 -0400

    Update logisticaService.js

--- servicio-repartidor/src/services/logisticaService.js
+++ servicio-repartidor/src/services/logisticaService.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 class LogisticaService {
     
-    // 1. Funciones exclusivas de dominio de Repartidores
+    // Funciones de dominio de Repartidores
     registrarRepartidor(datos) {
         const nuevoRep = new Repartidor(datos.id, datos.capacidad);
         return repartidorRepository.saveRepartidor(nuevoRep);
@@ -14,9 +14,9 @@ class LogisticaService {
         return repartidorRepository.findAllRepartidores();
     }
 
-    // 2. Función de Integración (El puente entre los dos mundos)
+    // Función de Integración
     async asignarPedido(pedidoId) {
-        // A. Le pregunta al servicio central (puerto 3000) por el objeto real y su peso
+        // Le pregunta al servicio central (puerto 3000) por el objeto real y su peso
         const respuesta = await fetch(`http://127.0.0.1:3000/api/pedidos/${pedidoId}/tracking`);
         const infoPedido = await respuesta.json();
         
@@ -25,7 +25,7 @@ class LogisticaService {
             throw new Error(`El pedido está en estado ${infoPedido.estado} y no puede ser asignado`);
         }
 
-        // B. Busca en su propia base de datos local un camión disponible
+        // Busca en la base de datos local un camión disponible
         const repartidores = repartidorRepository.findAllRepartidores();
         const pesoDelPaquete = Number(infoPedido.peso);
         const disponible = repartidores.find(r => r.puedeCargar(pesoDelPaquete)); 
@@ -34,10 +34,10 @@ class LogisticaService {
             throw new Error("No hay repartidores con capacidad suficiente");
         }
 
-        // C. Altera la carga del repartidor de forma local
+        // Altera la carga del repartidor de forma local
         disponible.asignarCarga(pesoDelPaquete);
 
-        // D. Notifica de vuelta al servicio de pedidos para que actualice SU máquina de estados
+        // Notifica de vuelta al servicio de pedidos para que actualice SU máquina de estados
         await fetch(`http://127.0.0.1:3000/api/pedidos/${pedidoId}/forzar-asignacion`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
